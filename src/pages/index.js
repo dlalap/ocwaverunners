32be6088ebfe8cbdef46c2d30e3e3ac097466251
Dean Lalap <deanlalap@gmail.com>
2020-08-02 02:59:28 -0700

    updated gatsby-config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+import "./style.css"
+// import Logo from "../logo"
+import Logo from "../images/wrhorizontal_white.png"
+// import Header from "../header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="app">
+      <img src={Logo} alt="Wave Runners Logo" width={500} />
+
+      <ul className="front-page-nav">
+        <li>
+          <a href="https://instagram.com/ocwaverunners/">Instagram </a>
+        </li>
+        <li>
+          <Link to="/page2">About</Link>
+        </li>
+        <li>
+          <Link to="/page2">Contact</Link>
+        </li>
+      </ul>
+      {/* <Link to="/page2">Link.</Link> */}
+    </div>
+  )
 }
